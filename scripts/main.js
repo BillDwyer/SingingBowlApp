1fe0e8c77e136e5da0f0bbb549af287d72c285ba
BillDwyer <BillDwyer@users.noreply.github.com>
2020-09-20 20:56:24 -0400

    Increased size of circle.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
 ctx.beginPath();
-ctx.arc(75, 75, 50, 0, 2 * Math.PI);
+ctx.arc(250, 250, 250, 0, 2 * Math.PI);
 ctx.fill();
