33bde4423b35ef72afef7986164b9093b24e0e9a
BillDwyer <BillDwyer@users.noreply.github.com>
2020-09-26 19:58:12 -0400

    Change color mouse over and click on circle

--- scripts/main.js
+++ scripts/main.js
@@ -9,25 +9,42 @@ window.addEventListener('resize', function () {
 
 class SingingBowl {
   constructor() {
+    this.color = 'rgba(0, 0, 0, 1)'
+    this.bowl = new Path2D();
+    this.bowl.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
   }
   draw = () => {
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)'
-    ctx.beginPath();
-    ctx.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
-    if (ctx.isPointInPath(mouse.x, mouse.y)){
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+    ctx.fillStyle = this.color;
+    if (ctx.isPointInPath(this.bowl,mouse.x, mouse.y)){
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     };
-    ctx.fill();
+    ctx.fill(this.bowl);
   }
   update = () => {
 
-  };
+  }
+  ring = () => {
+    if (ctx.isPointInPath(this.bowl,mouse.x, mouse.y)){
+      if (this.color === 'rgba(0, 0, 0, 1)'){
+        this.color = 'rgba(0,255,0,1)'
+      }
+      else{
+        this.color = 'rgba(0, 0, 0, 1)';
+      };
+    };
+
+  }
 }
 const bowl = new SingingBowl();
 let mouse = {
   x: undefined,
   y: undefined
 };
+
+canvas.addEventListener("mouseup", function (e) {
+  bowl.ring()
+}, false);
+
 
 window.addEventListener('mousemove', function (e) {
   mouse.x = event.x;
