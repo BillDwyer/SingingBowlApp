8c6fda18e30700d3bb13180f22fc12f78e8da054
BillDwyer <BillDwyer@users.noreply.github.com>
2020-09-26 19:19:50 -0400

    Fixed code to display correct color circle

--- scripts/main.js
+++ scripts/main.js
@@ -6,23 +6,20 @@ window.addEventListener('resize', function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
-//ctx.beginPath();
-//ctx.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
-//ctx.fill();
 
 class SingingBowl {
   constructor() {
-  };
+  }
   draw = () => {
-    ctx.fillStyle = 'rgba(255, 255, 255, 1)'
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)'
     ctx.beginPath();
     ctx.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
     ctx.fill();
-  };
+  }
   update = () => {
 
   };
-};
+}
 const bowl = new SingingBowl();
 let mouse = {
   x: undefined,
@@ -36,11 +33,7 @@ window.addEventListener('mousemove', function (e) {
 
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  ctx.fillStyle = 'rgba(255, 255, 255, 1)'
-  ctx.beginPath();
-  ctx.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
-  ctx.fill();
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   bowl.draw();
 };
 
