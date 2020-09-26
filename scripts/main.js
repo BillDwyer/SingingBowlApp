8696173b2a4bafecc26370e85629156787c3076f
BillDwyer <BillDwyer@users.noreply.github.com>
2020-09-26 18:55:54 -0400

    Testing for GitHub

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,15 @@ window.addEventListener('resize', function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
-ctx.beginPath();
-ctx.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
-ctx.fill();
+//ctx.beginPath();
+//ctx.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
+//ctx.fill();
 
 class SingingBowl {
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
-
+  constructor() {
   };
   draw = () => {
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)'
     ctx.beginPath();
     ctx.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
     ctx.fill();
@@ -24,8 +22,8 @@ class SingingBowl {
   update = () => {
 
   };
-}
-
+};
+const bowl = new SingingBowl();
 let mouse = {
   x: undefined,
   y: undefined
@@ -35,3 +33,15 @@ window.addEventListener('mousemove', function (e) {
   mouse.x = event.x;
   mouse.y = event.y;
 });
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  ctx.fillStyle = 'rgba(255, 255, 255, 1)'
+  ctx.beginPath();
+  ctx.arc(Math.floor(canvas.width/2), Math.floor(canvas.height/2), Math.floor(Math.min(canvas.width/4,canvas.height/4)), 0, 2 * Math.PI);
+  ctx.fill();
+  bowl.draw();
+};
+
+animate();
